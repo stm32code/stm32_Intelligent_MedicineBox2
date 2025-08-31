78b167390dfd1b19bf80036d016a6c63cd9e0696
单片机物联网设计 <343449035@qq.com>
2025-08-31 15:02:44 +0800

    version 1

---
+++ 84.1.智能药盒（语音）4G模块版本/01.APP/smartmedicinebox/app/src/main/java/com/example/smartmedicinebox/entity/User.kt
@@ -0,0 +1,10 @@
+package com.example.smartmedicinebox.entity
+
+data class User(
+    var uid: Int? = null,
+    var uname: String? = null,
+    var upassword: String? = null,
+    var per: Int? = null,
+    var createDateTime: String? = null,
+    var phone: String? = null
+)
